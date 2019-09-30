4d50f206bfc163b15094b8924799fe6483678b33
Joe Gerhard <jlgerhard@gmail.com>
2019-09-30 10:47:40 -0500

    Add shoppin cart functionality

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { CategoryNavigation } from "./CategoryNavigation";
 import { ProductList } from "./ProductList";
+import { CartSummary } from "./CartSummary";
 
 export class Shop extends Component {
+
+  handleAddToCart = (...args) => {
+    this.props.addToCart(...args);
+    this.props.history.push("/shop/cart");
+  }
 
   render() {
     return (
@@ -10,6 +16,7 @@ export class Shop extends Component {
         <div className="row">
           <div className="col bg-dark text-white">
             <div className="navbar-brand">SPORTS STORE</div>
+            <CartSummary { ...this.props } />
           </div>
         </div>
         <div className="row">
@@ -18,7 +25,8 @@ export class Shop extends Component {
               categories={ this.props.categories } />
           </div>
           <div className="col-9 p-2">
-            <ProductList products={ this.props.products } />
+            <ProductList products={ this.props.products }
+              addToCart={ this.handleAddToCart } />
           </div>
         </div>
       </div>
